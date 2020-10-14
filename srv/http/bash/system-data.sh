a1d1fd8799092b592f0dbf5083ec0b4c9191fe6d
rern <rernrern@gmail.com>
2020-10-14 20:17:13 +0700

    Update system-data.sh

--- srv/http/bash/system-data.sh
+++ srv/http/bash/system-data.sh
@@ -24,10 +24,7 @@ bullet='<gr> &bull; </gr>'
 cpuload=$( cat /proc/loadavg | cut -d' ' -f1-3 | sed 's/ /\&emsp;/g' )
 cputemp=$( printf "%.0f\n" $( /opt/vc/bin/vcgencmd measure_temp | cut -d= -f2 | cut -d\' -f1 ) )
 date=( $( date +'%T %F' ) )
-startup=( $( systemd-analyze | head -1 | cut -d' ' -f4,7 | tr -d s ) )
-skernel=$( printf "%.0f\n" ${startup[0]} )
-suser=$( printf "%.0f\n" ${startup[1]} )
-startup="${skernel}s <gr>(kernel)</gr> + ${suser}s <gr>(userspace)</gr> = $(( skernel + suser ))s"
+startup=$( systemd-analyze | head -1 | cut -d' ' -f4,7 | tr -d s )
 timezone=$( timedatectl | awk '/zone:/ {print $3}' )
 time="${date[0]}$bullet${date[1]}&emsp;<grw>${timezone//\// &middot; }</grw>"
 uptime=$( uptime -p | tr -d 's,' | sed 's/up //; s/ day/d/; s/ hour/h/; s/ minute/m/' )
