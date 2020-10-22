804a9a71f8979a33043773bbab2630580ffb621c
rern <rernrern@gmail.com>
2020-10-22 09:32:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ installstart "$1"
 getinstallzip
 
 if [[ -e /usr/bin/bluetoothctl && ! -e /etc/systemd/system/bluez-authorize.service ]]; then
-	pacman -Sy python-dbus python-gobject
+	pacman -Sy --needed python-dbus python-gobject
 	echo '[Unit]
 Description=Bluetooth auto authorization
 After=bluetooth.service
@@ -18,7 +18,7 @@ Requires=bluetooth.service
 [Service]
 Type=Idle
 ExecStart=/srv/http/bash/bluez_authorize.py' > /etc/systemd/system/bluez-authorize.service
-	sed -i '/BindsTo/ s/$/ bluez-authorize.service/' /etc/systemd/system/bluetooth.service.d/override.conf
+	sed -i 's/\(aplay.service\).*/\1 bluez-authorize.service/' /etc/systemd/system/bluetooth.service.d/override.conf
 	systemctl daemon-reload
 	systemctl try-restart bluetooth
 fi
