2c6679cbf364c7e4496a50f4d31204d924d05207
rern <rernrern@gmail.com>
2020-10-22 11:03:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,19 +8,39 @@ installstart "$1"
 
 getinstallzip
 
-if [[ -e /usr/bin/bluetoothctl && ! -e /etc/systemd/system/bluez-authorize.service ]]; then
+file=/etc/systemd/system/bluez-authorize.service
+if [[ -e /usr/bin/bluetoothctl && ! -e $file ]]; then
 	pacman -Sy --needed python-dbus python-gobject
-	echo '[Unit]
+	echo "\
+[Unit]
 Description=Bluetooth auto authorization
 After=bluetooth.service
 Requires=bluetooth.service
 
 [Service]
 Type=Idle
-ExecStart=/srv/http/bash/bluez_authorize.py' > /etc/systemd/system/bluez-authorize.service
+ExecStart=/srv/http/bash/bluez_authorize.py
+" > $file
 	sed -i 's/\(aplay.service\).*/\1 bluez-authorize.service/' /etc/systemd/system/bluetooth.service.d/override.conf
 	systemctl daemon-reload
 	systemctl try-restart bluetooth
+fi
+
+file=/etc/systemd/system/wlan0-powersaveoff.service
+if [[ ! -e $file ]]; then
+	echo "\
+[Unit]
+Description=Set WiFi power save off
+After=sys-subsystem-net-devices-wlan0.device
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/iw wlan0 set power_save off
+
+[Install]
+WantedBy=sys-subsystem-net-devices-wlan0.device
+" > $file
+	systemctl enable wlan0-powersaveoff
 fi
 
 installfinish
