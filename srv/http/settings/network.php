b29c8a1c79de26b4b5c8df0a5566be1d5f84f50c
rern <rernrern@gmail.com>
2020-10-23 11:58:37 +0700

    Revert "u"
    This reverts commit 12312e354778804b89791e3a751badf0f0fcd6f4.

--- srv/http/settings/network.php
+++ srv/http/settings/network.php
@@ -3,7 +3,8 @@
 	<heading id="headlan" class="noline">LAN<i id="lanadd" class="fa fa-plus-circle"></i></heading>
 	<ul id="listlan" class="entries"></ul>
 <?php }
-	  if ( exec( 'ifconfig | grep ^wlan' ) ) { ?>
+	  $wlan = exec ( 'ifconfig | grep ^wlan' );
+	  if ( $wlan ) { ?>
 	<div>
 	<heading id="headwl" class="noline">Wi-Fi<i id="wladd" class="fa fa-plus-circle"></i><i id="wlscan" class="fa fa-search"></i><?=$help?></heading>
 	<ul id="listwl" class="entries"></ul>
@@ -61,7 +62,7 @@
 	<ul id="listbtscan" class="entries"></ul>
 </div>
 
-	<?php if ( exec( 'systemctl -q is-active hostapd && echo 1 || echo 0' ) ) { ?>
+	<?php if ( $wlan && file_exists( '/usr/bin/hostapd' ) ) { ?>
 <div id="divaccesspoint">
 	<heading>RPi Access Point<?=$help?></heading>
 	<div class="col-l">Enable</div>
