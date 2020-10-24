1d5668d1bb6c5ca81cd8806937c96e312216a79c
rern <rernrern@gmail.com>
2020-10-24 21:06:18 +0700

    u

--- install.sh
+++ install.sh
@@ -19,9 +19,17 @@ Requires=bluetooth.service
 
 [Service]
 Type=Idle
-ExecStart=/srv/http/bash/bluez_authorize.py
+ExecStart=/srv/http/bash/bluez-authorize.py
 " > /etc/systemd/system/bluez-authorize.service
 	sed -i 's/\(aplay.service\).*/\1 bluez-authorize.service/' /etc/systemd/system/bluetooth.service.d/override.conf
+	systemctl daemon-reload
+	systemctl try-restart bluetooth
+fi
+
+file=/etc/systemd/system/bluez-authorize.service
+if [[ -e $file ]] && grep -q bluez_authorize $file; then
+	sed -i 's/bluez_authorize/bluez-authorize/' $file
+	mv /srv/http/bash/bluez{_,-}authorize.py
 	systemctl daemon-reload
 	systemctl try-restart bluetooth
 fi
