77c1dbc9a83ec20e08ede1de1b943ddaeb7d1180
rern <rernrern@gmail.com>
2020-10-24 17:25:57 +0700

    Update bluez_authorize.py

--- srv/http/bash/bluez_authorize.py
+++ srv/http/bash/bluez_authorize.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python
 
-from __future__ import absolute_import, print_function, unicode_literals
-from optparse import OptionParser
-import sys
 import dbus
 import dbus.service
 import dbus.mainloop.glib
 from gi.repository import GLib
 
-BUS_NAME = 'org.bluez'
 AGENT_INTERFACE = 'org.bluez.Agent1'
-AGENT_PATH = "/test/autoagent"
-
-bus = None
 
 class Agent(dbus.service.Object):
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="os", out_signature="")
+	@dbus.service.method(AGENT_INTERFACE, in_signature='os', out_signature='')
 	def AuthorizeService(self, device, uuid):
 		return
 
-	@dbus.service.method(AGENT_INTERFACE,
-					in_signature="o", out_signature="")
+	@dbus.service.method(AGENT_INTERFACE, in_signature='o', out_signature='')
 	def RequestAuthorization(self, device):
 		return
 
@@ -29,34 +20,15 @@ if __name__ == '__main__':
 	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
 	bus = dbus.SystemBus()
-
-	capability = "NoInputNoOutput"
-
-	parser = OptionParser()
-	parser.add_option("-i", "--adapter", action="store",
-					type="string",
-					dest="adapter_pattern",
-					default=None)
-	parser.add_option("-c", "--capability", action="store",
-					type="string", dest="capability")
-	parser.add_option("-t", "--timeout", action="store",
-					type="int", dest="timeout",
-					default=60000)
-	(options, args) = parser.parse_args()
-	if options.capability:
-		capability  = options.capability
-
-	path = "/test/autoagent"
-	agent = Agent(bus, path)
-
+	path = '/test/autoagent'
+    
+	Agent(bus, path)
+    
 	mainloop = GLib.MainLoop()
 
-	obj = bus.get_object(BUS_NAME, "/org/bluez");
-	manager = dbus.Interface(obj, "org.bluez.AgentManager1")
-	manager.RegisterAgent(path, capability)
-
-	print("Agent registered")
-
+	obj = bus.get_object('org.bluez', '/org/bluez');
+	manager = dbus.Interface(obj, 'org.bluez.AgentManager1')
+	manager.RegisterAgent(path, 'NoInputNoOutput')
 	manager.RequestDefaultAgent(path)
 
 	mainloop.run()
