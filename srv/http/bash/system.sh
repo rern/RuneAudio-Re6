090a1a2b6da57156fbc1bfb67075c12a0a4e4499
rern <rernrern@gmail.com>
2020-10-29 08:01:46 +0700

    Update system.sh

--- srv/http/bash/system.sh
+++ srv/http/bash/system.sh
@@ -104,7 +104,7 @@ i2c-dev
 		sed -i 's/fb0/fb1/' /usr/share/X11/xorg.conf.d/99-fbturbo.conf
 	else
 		sed -i 's/ console=ttyAMA0.*ProFont6x11// d' /boot/cmdline.txt
-		sed -i 'hotplug/,/tft35a/ d' /boot/config.txt
+		sed -i '/i2c_arm=on/,/tft35a/ d' /boot/config.txt
 		sed -i '/i2c-bcm2708\|i2c-dev/ d' /etc/modules-load.d/raspberrypi.conf
 		sed -i 's/fb1/fb0/' /usr/share/X11/xorg.conf.d/99-fbturbo.conf
 	fi
