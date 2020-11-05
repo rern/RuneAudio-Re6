b823e53d6281acb2abb8fece13ba84059566511b
rern <rernrern@gmail.com>
2020-11-06 04:13:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ installstart "$1"
 
 getinstallzip
 
+[[ -e /usr/bin/upmpdcli ]] && pacman -R --noconfirm libnpupnp libupnpp upmpdcli
+pacman -Sy --noconfirm libnpupnp libupnpp upmpdcli
+
 installfinish
 
 restartlocalbrowser
