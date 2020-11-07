ab3ae1c0355dbf812a9d7342e3b4dd3c8d2a3ebf
rern <rernrern@gmail.com>
2020-11-07 09:15:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ getinstallzip
 if [[ $( upmpdcli -v 2> /dev/null | cut -d' ' -f2 ) == 1.4.14 ]]; then
 	pacman -R --noconfirm libnpupnp libupnpp upmpdcli
 	pacman -Sy --noconfirm libnpupnp libupnpp upmpdcli
-	grep -q upnpicon.png /etc/upmpdcli.conf.pacsave 2> /dev/null && mv -f /etc/upmpdcli.conf{.pacsave,}
 	systemctl try-restart upmpdcli
 fi
+grep -q upnpicon.png /etc/upmpdcli.conf.pacsave 2> /dev/null && mv -f /etc/upmpdcli.conf{.pacsave,}
 
 installfinish
 
