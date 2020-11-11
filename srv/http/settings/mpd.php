e117913edcb7e230d95ddfee18bf0c299c39329f
rern <rernrern@gmail.com>
2020-11-11 15:04:24 +0700

    Add files via upload

--- srv/http/settings/mpd.php
+++ srv/http/settings/mpd.php
@@ -137,18 +137,18 @@
 	<i id="setting-soxr" class="setting fa fa-gear"></i>
 	<span class="help-block hide">
 			SoX Resampler custom settings:
-		<br>&bull; Precision - Number of used bits <code>16, 20, 24, 28 or 32</code> (20 = HQ)
-		<br>&bull; Phase Response <code>0-100</code> (0 = Minimum, 50 = Linear)
-		<br>&bull; Passband End - Original bandwidth of source used <code>90-99.7</code> %
-		<br>&bull; Stopband Begin <code>&gt;100</code> %
+		<br>&bull; Precision - Conversion precision <code>16, 20, 24, 28 or 32</code> bits (20 = HQ)
+		<br>&bull; Phase Response <code>0-100</code> (50 = Linear)
+		<br>&bull; Passband End - 0dB pt. bandwidth to preserve; nyquist=1 <code>90-91.3</code> %
+		<br>&bull; Stopband Begin - Aliasing/imaging control <code>> Passband End</code> %
 		<br>&bull; Attenuation - Lowers the source to prevent clipping <code>0-30</code> dB
-		<br>&bull; Flags - Bit fields:
-		<br> &emsp; 00 = ROLLOFF_SMALL 0.01 dB
-		<br> &emsp; 01 = ROLLOFF_MEDIUM 0.35 dB
-		<br> &emsp; 02 = ROLLOFF_NONE For Chebyshev bandwidth
-		<br> &emsp; 08 = HI_PREC_CLOCK Increase irrational ratio accuracy
-		<br> &emsp; 16 = DOUBLE_PRECISION Use D.P. calcs even if precision <= 20
-		<br> &emsp; 32 = SOXR_VR Variable-rate resampling
+		<br>&bull; Flags - Bit fields: <code>0, 1, 2, 8, 16 or 32</code>
+		<br> &emsp; 0 = Rolloff <= 0.01 dB
+		<br> &emsp; 1 = Rolloff <= 0.35 dB
+		<br> &emsp; 2 = Rolloff none - For Chebyshev bandwidth
+		<br> &emsp; 8 = High precision clock - Increase irrational ratio accuracy
+		<br> &emsp; 16 = Use double precision calcs even if precision <= 20
+		<br> &emsp; 32 = Variable-rate resampling
 	</span>
 </div>
 </div>
