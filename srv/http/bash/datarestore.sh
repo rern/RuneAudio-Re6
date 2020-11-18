680eb778c7146e02d5f1d9e6f37de9833954b5fe
rern <rernrern@gmail.com>
2020-11-18 17:50:42 +0700

    Update datarestore.sh

--- srv/http/bash/datarestore.sh
+++ srv/http/bash/datarestore.sh
@@ -24,32 +24,79 @@ chown -R http:http /srv/http
 chown mpd:audio $dirdata/mpd/mpd* &> /dev/null
 chmod 755 /srv/http/* $dirbash/* /srv/http/settings/*
 
-# hostname
-if [[ $( cat $dirsystem/hostname ) != RuneAudio ]]; then
-	hostname=$( cat $dirsystem/hostname )
-	$dirbash/system.sh hostname$'\n'$hostname
-fi
+# features #################################################################
+# airplay
+[[ -e /usr/bin/shairport-sync && -e $dirsystem/airplay ]] && $dirbash/features.sh airplay$'\n'true
+# spotify
+if [[ -e $dirsystem/spotify ]]; then
+	if [[ -e $dirsystem/spotifyset ]]; then
+		$dirbash/features.sh spotifyset$'\n'"$( cat $dirsystem/spotifyset )"
+	else
+		$dirbash/features.sh spotify$'\n'true
+	fi
+# upmpdcli
+[[ -e /usr/bin/upmpdcli && -e $dirsystem/upnp ]] && $dirbash/features.sh upnp$'\n'true
+# snapcast
+[[ -e $dirsystem/snapcast ]] && $dirbash/features.sh snapcast$'\n'true
 # chromium
-[[ ! -e $dirsystem/localbrowser ]] && $dirbash/features.sh localbrowser$'\n'false
-[[ -e $dirsystem/localbrowserset ]] && $dirbash/features.sh localbrowserset$'\n'"$( cat $dirsystem/localbrowserset )"
-[[ -e $dirsystem/calibration ]] && cp -f $dirsystem/calibration /etc/X11/xorg.conf.d/99-calibration.conf
-# color
-[[ -e $dirsystem/color ]] && color=1 && $dirbash/cmd.sh color
-# fstab
-if ls $dirsystem/fstab-* &> /dev/null; then
-	sed -i '\|/mnt/MPD/NAS| d' /etc/fstab
-	rmdir /mnt/MPD/NAS/* &> /dev/null
-	files=( $dirsystem/fstab-* )
-	for file in "${files[@]}"; do
-		cat $file >> /etc/fstab
-		mkdir -p "/mnt/MPD/NAS/${file/*fstab-}"
-	done
-	mount -a
+if [[ ! -e $dirsystem/localbrowser ]]; then
+	if [[ -e $dirsystem/localbrowserset ]]; then
+		$dirbash/features.sh localbrowserset$'\n'"$( cat $dirsystem/localbrowserset )"
+	else
+		$dirbash/features.sh localbrowser$'\n'false
+	fi
 fi
-# hostapd
-[[ -e $dirsystem/accesspoint ]] && $dirbash/features.sh accesspointset$'\n'"$( cat $dirsystem/accesspoint )"
+[[ -e $dirsystem/calibration ]] && cp -f $dirsystem/calibration /etc/X11/xorg.conf.d/99-calibration.conf
+# samba
+if [[ -e $dirsystem/samba ]]; then
+	if [[ -e $dirsystem/sambaset ]]; then
+		$dirbash/features.sh sambaset$'\n'"$( cat $dirsystem/sambaset )"
+	else
+		$dirbash/features.sh samba$'\n'true
+	fi
+fi
+# mpdscribble
+[[ -e $dirsystem/mpdscribble ]] && $dirbash/features.sh mpdscribbleset$'\n'"$( cat $dirsystem/mpdscribbleset )"
 # login
 [[ -e $dirsystem/login ]] && $dirbash/features.sh login$'\n'true
+# accesspoint
+if [[ -e $dirsystem/accesspoint ]]; then
+	if [[ -e $dirsystem/accesspointset ]]; then
+		$dirbash/features.sh accesspointset$'\n'"$( cat $dirsystem/accesspointset )"
+	else
+		$dirbash/features.sh accesspoint$'\n'true
+	fi
+
+# system #################################################################
+# onboard bluetooth
+[[ -e $dirsystem/onboard-bluetooth ]] && $dirbash/system.sh bluetooth$'\n'true
+# onboard wifi
+[[ ! -e $dirsystem/onboard-wlan ]] && $dirbash/system.sh wlan$'\n'false
+# i2s audio
+aplayname=$( cat $dirsystem/audio-aplayname 2> /dev/null )
+output=$( cat $dirsystem/audio-output )
+grep -q "$output.*$aplayname" /srv/http/settings/system-i2s.json && $dirbash/system.sh i2smodule$'\n'"$aplayname"$'\n'"$output"
+# onboard audio
+[[ ! -e $dirsystem/onboard-audio ]] && $dirbash/system.sh onboardaudio$'\n'false
+# 480x320 lcd
+[[ -e $dirsystem/lcd ]] && $dirbash/system.sh lcd lcd$'\n'true
+# character lcd
+if [[ -e $dirsystem/lcdchar ]]; then
+	if [[ -e $dirsystem/lcdcharset ]]; then
+		$dirbash/system.sh lcdcharset$'\n'"$( cat $dirsystem/lcdcharset )"
+	else
+		$dirbash/system.sh lcdchar$'\n'true
+	fi
+# hostname
+[[ $( cat $dirsystem/hostname ) != RuneAudio ]] && $dirbash/system.sh hostname$'\n'$( cat $dirsystem/hostname )
+# timezone
+[[ -e $dirsystem/timezone ]] && timedatectl set-timezone $( cat $dirsystem/timezone )
+# regional
+[[ -e $dirsystem/regional ]] && $dirbash/features.sh regional$'\n'"$( cat $dirsystem/regional )"
+# soundprofile
+[[ -e $dirsystem/soundprofile ]] && $dirbash/system.sh soundprofileset$'\n'"$( cat $dirsystem/soundprofileset )"
+
+# mpd #################################################################
 # mpd.conf
 file=$dirsystem/mpd
 if ls $file-* &> /dev/null; then
@@ -65,38 +112,19 @@ if ls $file-* &> /dev/null; then
 	rm /srv/http/data/shm/datarestore
 	/srv/http/bash/mpd-conf.sh
 fi
-# mpdscribble
-[[ -e $dirsystem/mpdscribble ]] && $dirbash/features.sh mpdscribbleset$'\n'"$( cat $dirsystem/mpdscribbleset )"
-# regional
-[[ -e $dirsystem/regional ]] && $dirbash/features.sh regional$'\n'"$( cat $dirsystem/regional )"
-# samba
-[[ -e $dirsystem/sambaset ]] && $dirbash/features.sh samba$'\n'"$( cat $dirsystem/sambaset )"
-[[ -e $dirsystem/samba ]] && $dirbash/features.sh sambaset$'\n'true
-# shairport-sync
-[[ -e /usr/bin/shairport-sync && -e $dirsystem/airplay ]] && $dirbash/features.sh airplay$'\n'true
-# snapcast
-[[ -e $dirsystem/snapcast ]] && $dirbash/features.sh snapcast$'\n'true
-[[ -e $dirsystem/snapclientset ]] && $dirbash/features.sh snapclientset$'\n'"$( cat $dirsystem/snapclientset )"
-# spotify
-[[ -e $dirsystem/spotify ]] && $dirbash/features.sh spotify$'\n'true
-[[ -e $dirsystem/spotifyset ]] && $dirbash/features.sh spotifyset$'\n'"$( cat $dirsystem/spotify-device )"
-# timezone
-[[ -e $dirsystem/timezone ]] && timedatectl set-timezone $( cat $dirsystem/timezone )
-# upmpdcli
-[[ -e /usr/bin/upmpdcli && -e $dirsystem/upnp ]] && $dirbash/features.sh upnp$'\n'true
 
-### config.txt
-# onboard bluetooth
-[[ -e $dirsystem/onboard-bluetooth ]] && $dirbash/system.sh bluetooth$'\n'true
-# onboard wifi
-[[ ! -e $dirsystem/onboard-wlan ]] && $dirbash/system.sh wlan$'\n'false
-# i2s audio
-aplayname=$( cat $dirsystem/audio-aplayname 2> /dev/null )
-output=$( cat $dirsystem/audio-output )
-grep -q "$output.*$aplayname" /srv/http/settings/system-i2s.json && $dirbash/system.sh i2smodule$'\n'"$aplayname"$'\n'"$output"
-# onboard audio
-[[ ! -e $dirsystem/onboard-audio ]] && $dirbash/system.sh onboardaudio$'\n'false
+# sources #################################################################
+# fstab
+if ls $dirsystem/fstab-* &> /dev/null; then
+	sed -i '\|/mnt/MPD/NAS| d' /etc/fstab
+	rmdir /mnt/MPD/NAS/* &> /dev/null
+	files=( $dirsystem/fstab-* )
+	for file in "${files[@]}"; do
+		cat $file >> /etc/fstab
+		mkdir -p "/mnt/MPD/NAS/${file/*fstab-}"
+	done
+	mount -a
+fi
 
-[[ -e $dirsystem/lcdchar ]] && $dirbash/system.sh lcdcharset$'\n'"$( cat $dirsystem/lcdchar )"
-
-[[ -e $dirsystem/lcd ]] && $dirbash/system.sh lcd lcd$'\n'true
+# color
+[[ -e $dirsystem/color ]] && color=1 && $dirbash/cmd.sh color
