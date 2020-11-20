c0642b4d98947826f969addca107c45386cde39f
rern <rernrern@gmail.com>
2020-11-21 02:22:01 +0700

    Update features-data.sh

--- srv/http/bash/features-data.sh
+++ srv/http/bash/features-data.sh
@@ -15,7 +15,7 @@ data+='
 	, "snapserver"      : '$( systemctl -q is-active snapserver && echo true || echo false )'
 	, "snapclient"      : '$( [[ -e $dirsystem/snapclient ]] && echo true || echo false )'
 	, "snapclientset"   : '$( cat $dirsystem/snapclientset 2> /dev/null || echo false )'
-	, "snaplatency"     : "'$( grep OPTS= /etc/default/snapclient | sed 's/.*latency=\(.*\)"/\1/' )'"
+	, "snaplatency"     : '$( grep OPTS= /etc/default/snapclient | sed 's/.*latency="\(.*\)"/\1/' )'
 	, "snappassword"    : "'$( cat $dirsystem/snapclientpw 2> /dev/null )'"
 	, "streaming"       : '$( grep -q 'type.*"httpd"' /etc/mpd.conf && echo true || echo false )
 # hostapd
