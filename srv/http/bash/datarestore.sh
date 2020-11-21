f23d2b6577f6838a4de5195d6f604478b67368d5
rern <rernrern@gmail.com>
2020-11-21 15:51:59 +0700

    Add files via upload

--- srv/http/bash/datarestore.sh
+++ srv/http/bash/datarestore.sh
@@ -25,31 +25,8 @@ chown mpd:audio $dirdata/mpd/mpd* &> /dev/null
 chmod 755 /srv/http/* $dirbash/* /srv/http/settings/*
 
 # features #################################################################
-# accesspoint
-[[ -e $dirsystem/hostapdset ]] && $dirbash/features.sh hostapdset$'\n'"$( cat $dirsystem/hostapdset )"
-[[ -e $dirsystem/hostapd ]] && $dirbash/features.sh hostapd$'\n'true
-
 # airplay
 [[ -e $dirsystem/shairport-sync ]] && $dirbash/features.sh shairport-sync$'\n'true
-
-# localbrowser
-[[ ! -e $dirsystem/localbrowser ]] && $dirbash/features.sh localbrowser$'\n'false
-[[ -e $dirsystem/localbrowserset ]] && $dirbash/features.sh localbrowserset$'\n'"$( cat $dirsystem/localbrowserset )"
-
-# login
-[[ -e $dirsystem/loginset ]] && $dirbash/features.sh loginset$'\n'"$( cat $dirsystem/loginset )"
-[[ -e $dirsystem/login ]] && $dirbash/features.sh login$'\n'true
-
-# mpdscribble
-[[ -e $dirsystem/mpdscribbleset ]] && $dirbash/features.sh mpdscribbleset$'\n'"$( cat $dirsystem/mpdscribbleset )"
-[[ -e $dirsystem/mpdscribble ]] && $dirbash/features.sh mpdscribble$'\n'true
-
-# samba
-[[ -e $dirsystem/smbset ]] && $dirbash/features.sh smbset$'\n'"$( cat $dirsystem/smbset )"
-[[ -e $dirsystem/smb ]] && $dirbash/features.sh smb$'\n'true
-
-# snapcast
-[[ -e $dirsystem/snapserver ]] && $dirbash/features.sh snapserver$'\n'true
 
 # snapclient
 [[ -e $dirsystem/snapclientset ]] && $dirbash/features.sh snapclientset$'\n'"$( cat $dirsystem/snapclientset )"
@@ -62,14 +39,40 @@ chmod 755 /srv/http/* $dirbash/* /srv/http/settings/*
 # upnp
 [[ -e $dirsystem/upmpdcli ]] && $dirbash/features.sh upmpdcli$'\n'true
 
+# snapserver
+[[ -e $dirsystem/snapserver ]] && $dirbash/features.sh snapserver$'\n'true
+
+# localbrowser
+[[ ! -e $dirsystem/localbrowser ]] && $dirbash/features.sh localbrowser$'\n'false
+[[ -e $dirsystem/localbrowserset ]] && $dirbash/features.sh localbrowserset$'\n'"$( cat $dirsystem/localbrowserset )"
+
+# smb
+[[ -e $dirsystem/smbset ]] && $dirbash/features.sh smbset$'\n'"$( cat $dirsystem/smbset )"
+[[ -e $dirsystem/smb ]] && $dirbash/features.sh smb$'\n'true
+
+# mpdscribble
+[[ -e $dirsystem/mpdscribbleset ]] && $dirbash/features.sh mpdscribbleset$'\n'"$( cat $dirsystem/mpdscribbleset )"
+[[ -e $dirsystem/mpdscribble ]] && $dirbash/features.sh mpdscribble$'\n'true
+
+# login
+[[ -e $dirsystem/loginset ]] && $dirbash/features.sh loginset$'\n'"$( cat $dirsystem/loginset )"
+[[ -e $dirsystem/login ]] && $dirbash/features.sh login$'\n'true
+
+# accesspoint
+[[ -e $dirsystem/hostapdset ]] && $dirbash/features.sh hostapdset$'\n'"$( cat $dirsystem/hostapdset )"
+[[ -e $dirsystem/hostapd ]] && $dirbash/features.sh hostapd$'\n'true
+
 # system #################################################################
+# onboardaudio
+[[ ! -e $dirsystem/onboard-audio ]] && $dirbash/system.sh onboardaudio$'\n'false
+
 # bluetooth
 [[ -e $dirsystem/onboard-bluetooth ]] && $dirbash/system.sh bluetooth$'\n'true
 
-# hostname
-[[ $( cat $dirsystem/hostname ) != RuneAudio ]] && $dirbash/system.sh hostname$'\n'$( cat $dirsystem/hostname )
+# wlan
+[[ ! -e $dirsystem/onboard-wlan ]] && $dirbash/system.sh wlan$'\n'false
 
-# i2smodule
+# audio i2s
 aplayname=$( cat $dirsystem/audio-aplayname 2> /dev/null )
 output=$( cat $dirsystem/audio-output )
 grep -q "$output.*$aplayname" /srv/http/settings/system-i2s.json && $dirbash/system.sh i2smodule$'\n'"$aplayname"$'\n'"$output"
@@ -82,8 +85,11 @@ grep -q "$output.*$aplayname" /srv/http/settings/system-i2s.json && $dirbash/sys
 [[ -e $dirsystem/lcdcharset ]] && $dirbash/system.sh lcdcharset$'\n'"$( cat $dirsystem/lcdcharset )"
 [[ -e $dirsystem/lcdchar ]] && $dirbash/system.sh lcdchar$'\n'true
 
-# onboardaudio
-[[ ! -e $dirsystem/onboard-audio ]] && $dirbash/system.sh onboardaudio$'\n'false
+# hostname
+[[ $( cat $dirsystem/hostname ) != RuneAudio ]] && $dirbash/system.sh hostname$'\n'$( cat $dirsystem/hostname )
+
+# timezone
+[[ -e $dirsystem/timezone ]] && $dirbash/system.sh timezone$'\n'"$( cat $dirsystem/timezone )"
 
 # regional
 [[ -e $dirsystem/regional ]] && $dirbash/features.sh regional$'\n'"$( cat $dirsystem/regional )"
@@ -92,26 +98,20 @@ grep -q "$output.*$aplayname" /srv/http/settings/system-i2s.json && $dirbash/sys
 [[ -e $dirsystem/soundprofileset ]] && $dirbash/system.sh soundprofileset$'\n'"$( cat $dirsystem/soundprofileset )"
 [[ -e $dirsystem/soundprofile ]] && $dirbash/system.sh soundprofile$'\n'true
 
-# timezone
-[[ -e $dirsystem/timezone ]] && $dirbash/system.sh timezone$'\n'"$( cat $dirsystem/timezone )"
-
-# wlan
-[[ ! -e $dirsystem/onboard-wlan ]] && $dirbash/system.sh wlan$'\n'false
-
 # mpd #################################################################
 # mpd.conf
 file=$dirsystem/mpd
 if ls $file-* &> /dev/null; then
 	touch /srv/http/data/shm/datarestore
-	[[ -e $file-autoupdate ]] &&    $dirbash/mpd.sh autoupdate$'\n'true
-	[[ -e $file-buffer ]] &&        $dirbash/mpd.sh bufferset$'\n'$( cat $file-buffer )
-	[[ -e $file-bufferoutput ]] &&  $dirbash/mpd.sh bufferoutputset$'\n'$( cat $file-bufferoutput )
 	[[ -e $file-crossfade ]] &&     $dirbash/mpd.sh crossfadeset$'\n'$( cat $file-crossfade )
-	[[ -e $file-custom ]] &&        $dirbash/mpd.sh customset$'\n'$( cat $file-custom-global )
-	[[ -e $file-ffmpeg ]] &&        $dirbash/mpd.sh ffmpeg$'\n'true
 	[[ -e $file-normalization ]] && $dirbash/mpd.sh normalization$'\n'true
 	[[ -e $file-replaygain ]] &&    $dirbash/mpd.sh replaygainset$'\n'$( cat $file-replaygain )
+	[[ -e $file-autoupdate ]] &&    $dirbash/mpd.sh autoupdate$'\n'true
+	[[ -e $file-ffmpeg ]] &&        $dirbash/mpd.sh ffmpeg$'\n'true
+	[[ -e $file-buffer ]] &&        $dirbash/mpd.sh bufferset$'\n'$( cat $file-buffer )
+	[[ -e $file-bufferoutput ]] &&  $dirbash/mpd.sh bufferoutputset$'\n'$( cat $file-bufferoutput )
 	[[ -e $file-soxr ]] &&          $dirbash/mpd.sh soxrset$'\n'$( cat $file-soxrset )
+	[[ -e $file-custom ]] &&        $dirbash/mpd.sh customset$'\n'$( cat $file-custom-global )
 	rm /srv/http/data/shm/datarestore
 	/srv/http/bash/mpd-conf.sh
 fi
@@ -136,4 +136,4 @@ fi
 # color
 [[ -e $dirsystem/color ]] && color=1 && $dirbash/cmd.sh color
 
-curl -s -X POST http://127.0.0.1/pub?id=refresh -d '{ "page": "system" }'
+curl -s -X POST http://127.0.0.1/pub?id=refresh -d '{ "page": "all" }'
