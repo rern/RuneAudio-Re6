0aa68e9957d58e20b86101074e9b75d8b7eca7bb
rern <rernrern@gmail.com>
2020-11-28 11:03:05 +0700

    Update system.js

--- srv/http/assets/js/system.js
+++ srv/http/assets/js/system.js
@@ -14,7 +14,7 @@ function renderStatus() {
 		+'<br>'+ G.startup.replace( ' ', ' <gr class="wide">(kernel)</gr> + ' ) +' <gr class="wide">(userspace)</gr>';
 	if ( G.undervdetected || G.undervoltage ) {
 		status += '<br><i style="width: 20px; text-align: center" class="fa fa-warning blink '
-				+ ( G.undervoltage ? red : '' ) +'"></i> Voltage under 4.7V'
+				+ ( G.undervoltage ? 'red' : '' ) +'"></i> Voltage under 4.7V'
 				+ ( G.undervdetected ? ' occured' : '' );
 	}
 	return status
